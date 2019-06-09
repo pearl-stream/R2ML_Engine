74e06af3d204235cf2a138635d032ae59fb8da22
freddy <fruether>
2019-06-09 14:10:42 +0200

    init

---
+++ src/main/java/Program.java
@@ -0,0 +1,12 @@
+
+/**
+ * Created by Frederik on 21.06.2017.
+ */
+public class Program {
+    public static void main(String[] args)  {
+       
+        System.out.println("I exist");
+       
+    }
+}
+
---
+++ src/test/java/Services/SQLServiceTest.java
@@ -0,0 +1,28 @@
+package Services;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.Set;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.*;
+
+/**
+ * Created by freddy on 15.10.17.
+ */
+public class SQLServiceTest {
+	
+	@Before
+	public void setUp() {
+		System.out.println("Doing something");
+	}
+	@Test
+	public void parseMySQL_correct() {
+			assertTrue(1==1);
+	}
+}
+
+
+
